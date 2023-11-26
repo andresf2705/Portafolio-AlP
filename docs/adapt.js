6d357839e2ec29207a572e943ce4e159c5db4cbe
Andrés Felipe <126476766+andresf2705@users.noreply.github.com>
2023-11-26 16:08:16 -0500

    Actualizacion de matenimiendo
    version 1.0.0.1

--- docs/adapt.js
+++ docs/adapt.js
@@ -1,20 +1,18 @@
-// Contenido de adapt.js
 
-// Script para mejorar la adaptabilidad y el scroll
 document.addEventListener("DOMContentLoaded", function() {
-    // Asegurar que el contenido sea visible después de cargar
+    
     document.body.style.display = "block";
 
-    // Agregar clases de scroll
+    
     let lastScroll = 0;
     window.addEventListener("scroll", function() {
         const currentScroll = window.scrollY;
         if (currentScroll > lastScroll) {
-            // Hacer scroll hacia abajo
+            
             document.body.classList.add("scroll-down");
             document.body.classList.remove("scroll-up");
         } else {
-            // Hacer scroll hacia arriba
+            
             document.body.classList.add("scroll-up");
             document.body.classList.remove("scroll-down");
         }
